bce885ce97471fb73c71f560284c607110af7c07
Julia Kondratsiuk <kondrlias@gmail.com>
2025-09-03 16:23:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.current = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.current = Math.round((min + max) / 2);
+  }
 
-  lower() { }
+  guess() {
+    return this.current;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.current;
+    this.current = Math.round((this.min + this.max) / 2);
+  }
+
+  greater() {
+    this.min = this.current;
+    this.current = Math.round((this.min + this.max) / 2);
+  }
 }
-
 module.exports = GuessingGame;
